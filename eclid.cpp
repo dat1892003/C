198fb6f58f5ffb2abb30906fcff99bb72ba2efcd
dat1892003 <lethanhdat1892003@gmail.com>
2025-09-19 21:04:24 +0700

    update

---
+++ eclid.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+#include <math.h>
+using namespace std;
+int main(){
+	long x,y,z,t;
+	cin>>x>>y>>z>>t;
+	if(x<-1000000000|| x>1000000000|| y<-1000000000 || y>1000000000 
+	|| z<-1000000000 || z>1000000000 || t<-1000000000 || t>1000000000){
+	
+	return 0;
+}
+ cout<<fixed<<setprecision(9)<<sqrt(pow(z-x,2)+pow(t-y,2));
+
+	return 0;
+}
